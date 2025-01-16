5408e9ba0ccd3ea27f15c46ded7148d34f5ed631
nhi1e <tnhile2004@gmail.com>
2025-01-15 17:45:14 -0800

    added clear button

--- asg1/src/ColoredPoints.js
+++ asg1/src/ColoredPoints.js
@@ -75,6 +75,10 @@ function addActionsForHtmlUI() {
 		g_selectedColor = [1.0, 0.0, 0.0, 1.0];
 	};
 
+	document.getElementById("clear").onclick = function () {
+		g_shapesList = [];
+		renderAllShapes();
+	}
 	document.getElementById("redSlide").addEventListener("mouseup", function () {
 		g_selectedColor[0] = this.value / 100;
 	});
