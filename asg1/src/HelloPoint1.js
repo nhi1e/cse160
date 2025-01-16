41b5ac6e360f4efae0da68bf3a886fdfafe3a087
nhi1e <tnhile2004@gmail.com>
2025-01-15 16:35:25 -0800

    done w cleanup

--- asg1/src/HelloPoint1.js
+++ asg1/src/HelloPoint1.js
@@ -55,7 +55,7 @@ function connectVariablesToGLSL() {
 
 function main() {
 	setupWebGL();
-  connectVariablesToGLSL(); 
+	connectVariablesToGLSL();
 
 	// Register function (event handler) to be called on a mouse press
 	canvas.onmousedown = click;
@@ -70,15 +70,12 @@ function main() {
 var g_points = []; // The array for the position of a mouse press
 var g_colors = []; // The array to store the color of a point
 function click(ev) {
-	var x = ev.clientX; // x coordinate of a mouse pointer
-	var y = ev.clientY; // y coordinate of a mouse pointer
-	var rect = ev.target.getBoundingClientRect();
-
-	x = ((x - rect.left) - canvas.width / 2) / (canvas.width / 2);
-	y = (canvas.height / 2 - (y - rect.top)) / (canvas.height / 2);
+	//extract event click and return it in GL coordinates
+	let [x, y] = convertCoordinatesEventToGL(ev);
 
 	// Store the coordinates to g_points array
 	g_points.push([x, y]);
+  
 	// Store the coordinates to g_points array
 	if (x >= 0.0 && y >= 0.0) {
 		// First quadrant
@@ -90,7 +87,21 @@ function click(ev) {
 		// Others
 		g_colors.push([1.0, 1.0, 1.0, 1.0]); // White
 	}
+	// draw every shape that is supposed to be rendered on canvas
+	renderAllShapes();
+}
 
+function convertCoordinatesEventToGL(ev) {
+	var x = ev.clientX; // x coordinate of a mouse pointer
+	var y = ev.clientY; // y coordinate of a mouse pointer
+	var rect = ev.target.getBoundingClientRect();
+
+	x = (x - rect.left - canvas.width / 2) / (canvas.width / 2);
+	y = (canvas.height / 2 - (y - rect.top)) / (canvas.height / 2);
+	return [x, y];
+}
+
+function renderAllShapes() {
 	// Clear <canvas>
 	gl.clear(gl.COLOR_BUFFER_BIT);
 
