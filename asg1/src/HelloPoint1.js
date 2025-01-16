4dd03da79845f90f106dc7a1a7913ce7d66db321
nhi1e <tnhile2004@gmail.com>
2025-01-15 17:03:34 -0800

    added color sliders

--- asg1/src/HelloPoint1.js
+++ asg1/src/HelloPoint1.js
@@ -53,9 +53,35 @@ function connectVariablesToGLSL() {
 	}
 }
 
+let g_selectedColor = [1.0, 1.0, 1.0, 1.0];
+
+//set up actions for html ui elements
+function addActionsForHtmlUI() {
+	//button events
+	document.getElementById("green").onclick = function () {
+		g_selectedColor = [0.0, 1.0, 0.0, 1.0];
+	};
+	document.getElementById("red").onclick = function () {
+		g_selectedColor = [1.0, 0.0, 0.0, 1.0];
+	};
+
+	document.getElementById("redSlide").addEventListener("mouseup", function () {
+		g_selectedColor[0] = this.value / 100;
+	});
+	document
+		.getElementById("greenSlide")
+		.addEventListener("mouseup", function () {
+			g_selectedColor[1] = this.value / 100;
+		});
+	document.getElementById("blueSlide").addEventListener("mouseup", function () {
+		g_selectedColor[2] = this.value / 100;
+	});
+}
+
 function main() {
 	setupWebGL();
 	connectVariablesToGLSL();
+	addActionsForHtmlUI();
 
 	// Register function (event handler) to be called on a mouse press
 	canvas.onmousedown = click;
@@ -75,18 +101,19 @@ function click(ev) {
 
 	// Store the coordinates to g_points array
 	g_points.push([x, y]);
-  
-	// Store the coordinates to g_points array
-	if (x >= 0.0 && y >= 0.0) {
-		// First quadrant
-		g_colors.push([1.0, 0.0, 0.0, 1.0]); // Red
-	} else if (x < 0.0 && y < 0.0) {
-		// Third quadrant
-		g_colors.push([0.0, 1.0, 0.0, 1.0]); // Green
-	} else {
-		// Others
-		g_colors.push([1.0, 1.0, 1.0, 1.0]); // White
-	}
+
+	g_colors.push(g_selectedColor.slice());
+	// // Store the coordinates to g_points array
+	// if (x >= 0.0 && y >= 0.0) {
+	// 	// First quadrant
+	// 	g_colors.push([1.0, 0.0, 0.0, 1.0]); // Red
+	// } else if (x < 0.0 && y < 0.0) {
+	// 	// Third quadrant
+	// 	g_colors.push([0.0, 1.0, 0.0, 1.0]); // Green
+	// } else {
+	// 	// Others
+	// 	g_colors.push([1.0, 1.0, 1.0, 1.0]); // White
+	// }
 	// draw every shape that is supposed to be rendered on canvas
 	renderAllShapes();
 }
